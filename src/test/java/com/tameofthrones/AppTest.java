a34f3fad2b69775b0d5b47f1098fade87dcf4df9
Satyam Ramani <satyamramani58554@gmail.com>
2020-08-02 20:29:56 +0530

    setup tame of thrones

---
+++ src/test/java/com/tameofthrones/AppTest.java
@@ -0,0 +1,22 @@
+package com.tameofthrones;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
