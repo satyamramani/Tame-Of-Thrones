c143ba878f95750ca9a4faf5ddb10077014ac7ff
Satyam Ramani <satyamramani58554@gmail.com>
2020-08-05 12:23:31 +0530

    git commit

--- src/main/java/com/tameofthrones/create/CreateKingdomsImp.java
+++ src/main/java/com/tameofthrones/create/CreateKingdomsImp.java
@@ -1,39 +1,36 @@
 package com.tameofthrones.create;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.tameofthrones.dto.Kingdom;
 
 public class CreateKingdomsImp implements CreateKingdoms {
 
+  Map<String,String> kingdomAndEmblem = new HashMap<String,String>();
   List<Kingdom> kingdoms = new ArrayList<Kingdom>();
 
-  final String filePath = "src/main/java/com/tameofthrones/resources/kingdom.txt";
+  public CreateKingdomsImp() {
+
+    kingdomAndEmblem.put("SPACE", "Gorilla");
+    kingdomAndEmblem.put("LAND", "Panda");
+    kingdomAndEmblem.put("AIR", "Owl");
+    kingdomAndEmblem.put("ICE", "Mammoth");
+    kingdomAndEmblem.put("WATER", "Octopus");
+    kingdomAndEmblem.put("FIRE", "Dragon");
+
+  }
 
   public List<Kingdom> getKingdoms() throws IOException {
 
-    String line;
-    //read file path
-    File file = new File(filePath);
-    FileReader fr = new FileReader(file);
-    BufferedReader br = new BufferedReader(fr);
+    for (String kingdomName : kingdomAndEmblem.keySet()) {
 
-    //read each line in the input file and seperate kingdom and its emblem using first space.
-    while((line = br.readLine()) != null) {
-
-      String kingdomName = line.substring(0,line.indexOf(' '));
-      String emblem = line.substring(line.indexOf(' ')+1).toUpperCase();
-
-      kingdoms.add(new Kingdom(kingdomName, emblem));
+      kingdoms.add(new Kingdom(kingdomName, kingdomAndEmblem.get(kingdomName).toUpperCase()));
 
     }
-    
-    br.close();
 
     return kingdoms;
   }
